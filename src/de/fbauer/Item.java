862d7249a054d7f1d9649827a13abb5158158084
TheBirdsAreSpies <shore@skreamyourlungsout.com>
2022-01-20 09:43:20 +0100

    jdocs are cool

--- src/de/fbauer/Item.java
+++ src/de/fbauer/Item.java
@@ -1,16 +1,28 @@
 package de.fbauer;
 
+/**
+ *
+ */
 public class Item {
    // Pokeball
    // Trank
    private String name;
    private double preis;
 
+   /**
+    * Creates a new item.
+    * @param name The name of the item
+    * @param preis The items price
+    */
    public Item(String name, double preis) {
       this.name = name;
       this.preis = preis;
    }
 
+   /**
+    * Returns the name of the item.
+    * @return Le name
+    */
    public String getName() {
       return this.name;
    }
